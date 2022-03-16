5c9cd7f444875aa515294068236875d57677da83
fromrupabali <mominkeno@gmail.com>
2022-03-17 01:08:12 +0600

    Successfully fetch data

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # request-multiple-urls
+
+A module used to fetch an array of URLs which contain JSON data and returns the response as a Promise Object.
+
+# Installation steps
+
+npm install request-multiple-urls
+
+# Example usage
+
+const getMultipleUrls = require("request-multiple-urls");
+
+const test_urls = [
+"https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/ftse-fsi.json",
+"https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-hkd.json",
+"https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-usd.json",
+];
+getMultipleUrls(test_urls).then((urlContent) => {
+console.log( urlContent);
+});
--- index.js
+++ index.js
@@ -9,24 +9,21 @@
 
 //all type requires
 const URL = require("url").URL;
-const https = require("https");
+const axios = require("axios");
 
-// Result
-let finalResult = [];
-
-//Error message
+//Error handle
 function handleError(err, message) {
-  if (typeof err === "object") {
-    if (err.message) {
-      console.log("\nMessage: " + message);
-    }
-    if (err.stack) {
-      console.log("\nStacktrace:");
-      console.log(err.stack);
-    }
-  } else {
-    console.log("Error :: argument is not an object");
+  // if (typeof err === "object") {
+  if (message) {
+    console.log("\nMessage: " + message);
   }
+  if (err.stack) {
+    console.log("\nStacktrace:");
+    console.log(err.stack);
+  }
+  setTimeout(function () {
+    process.exit(1);
+  }, 30);
 }
 
 //Check url is valid
@@ -43,62 +40,37 @@ const checkUrls = (urls) => {
   return result;
 };
 
-async function getResult(url, i, length) {
-  await https
-    .get(url, async (resp) => {
-      await resp.on("data", (data) => {
-        try {
-          const body = JSON.parse(data.toString("utf8"));
-          const result = {
-            url,
-            status: 200,
-            body,
-          };
-          // console.log("Result", result);
-          finalResult.push(result);
-          //   if (length == i) {
-          console.log(i, finalResult);
-          //  }
-        } catch (e) {
-          const result = {
-            url,
-            status: 200,
-            body: "Something wrong",
-          };
-          // console.log("Result", result);
-          // let message = e.concat("\n" + `at async Promise.all index(${i})`);
-          finalResult.push(e);
-          if (length == i) {
-            console.log(finalResult);
-          }
-        }
-      });
-      resp.on("end", () => {});
-    })
-    .on("error", (err) => {
-      console.log("Error: " + err.message);
+const getResult = async (url, i) => {
+  try {
+    return await axios.get(url).then((res) => {
+      if (res.headers["content-type"] == "application/json") {
+        const result = {
+          url,
+          status: 200,
+          data: res.data,
+        };
+        return result;
+      } else {
+        return {
+          url,
+          ErrorMessage: "Doesn't return a valid.",
+        };
+      }
     });
-}
+  } catch (err) {}
+};
+
 async function getMultipleUrls(urls) {
-  //Check url uis valid
-  //if valid find the response from url
-  // check the response
-  //if response is valid make reponse
-  //else throw error response
-  //else throw error response
+  if (urls.length === 0) {
+    return "Input URLs array must not be empty";
+  }
   const isValidUrls = checkUrls(urls);
   if (isValidUrls) {
-    urls.map(async (url, i) => {
-      await getResult(url, i, urls.length);
+    const promiseUrls = urls.map(async (url) => {
+      return await getResult(url);
     });
-    return finalResult;
+
+    return await Promise.all(promiseUrls);
   }
 }
-const test_urls = [
-  "https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/ftse-fsi.json",
-  "https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-hkd.json",
-  "https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-usd.json",
-];
-
-getMultipleUrls(test_urls);
 module.exports = getMultipleUrls;
